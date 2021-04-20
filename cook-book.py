2253332bfd335fee2828456e89b7e5c40a62b95d
mrGeist469 <75024897+mrGeist469@users.noreply.github.com>
2021-04-20 18:00:59 +0300

    add pprint

--- cook-book.py
+++ cook-book.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+
+
 def read():
     cook_book = {}
     with open('receipt.txt', encoding='utf-8') as file:
@@ -6,19 +9,17 @@ def read():
             count = int(file.readline())
             ingredients = []
             for i in range(count):
+                i += 1
                 ingredient_name, quantity, measure = file.readline().lower().strip().split(' | ')
                 ingredient = {
                     'ingredient_name': ingredient_name,
                     'quantity': int(quantity),
                     'measure': measure,
                 }
-                i += 1
-            ingredients.append(ingredient)
+                ingredients.append(ingredient)
             cook_book[dish_name] = ingredients
             file.readline()
     return cook_book
 
 
-print(read())
-
-
+pprint(read())
